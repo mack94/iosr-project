59a0cad6a711b65895b563ee40afd7ec6a47405b
maciej makówka <mack94@linux-ll5z.suse>
2017-11-08 16:42:51 +0100

    Hello somebody with swagger

---
+++ src/main/java/pl/edu/agh/iet/iosrproject/IosrProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.iet.iosrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IosrProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IosrProjectApplication.class, args);
+	}
+}
