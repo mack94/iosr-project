41e3141c7eb5681c6a11d17eefbb3d930c7d1b5a
lajonss <gfs.b16@gmail.com>
2018-01-30 00:01:09 +0100

    manipulator inclusion

--- iosr-project/src/main/java/pl/edu/agh/iet/iosrproject/hello/GreetingController.java
+++ iosr-project/src/main/java/pl/edu/agh/iet/iosrproject/hello/GreetingController.java
@@ -1,26 +1,28 @@
 package pl.edu.agh.iet.iosrproject.hello;
 
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(methods = { RequestMethod.GET, RequestMethod.OPTIONS })
 @RestController
 public class GreetingController {
-
+    private static final String MANIPULATOR_ADDRESS = "http://manipulator:1234/iosr/app/transform?value={value}";
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private long counter = 0l;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        counter = fetchNewCounter();
+        return new Greeting(counter, String.format(template, name));
     }
 
+    private long fetchNewCounter() {
+        RestTemplate restTemplate = new RestTemplate();
+        double result = restTemplate.getForObject(MANIPULATOR_ADDRESS, Double.class, counter);
+        return (long) result;
+    }
 }
