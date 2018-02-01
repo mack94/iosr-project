2bae9de3268d4e4fb4e6ac15ca915fdec8c805c4
Maciej Makowka <mack94@linux-nf1m.suse>
2018-02-01 13:32:52 +0100

    loggin support / but not working xD

--- iosr-project/src/main/java/pl/edu/agh/iet/iosrproject/IosrProjectApplication.java
+++ iosr-project/src/main/java/pl/edu/agh/iet/iosrproject/IosrProjectApplication.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.iet.iosrproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,7 +10,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class IosrProjectApplication {
 
+	static final Logger logger = LoggerFactory.getLogger(IosrProjectApplication.class);
+
 	public static void main(String[] args) {
+		logger.error("#1 IOSR Project is starting. Be careful! ");
+		logger.debug("#2 IOSR Project is starting. Be careful! ");
+		logger.info("#3 IOSR Project is starting. Be careful! ");
 		SpringApplication.run(IosrProjectApplication.class, args);
 	}
 }
--- runProject.sh
+++ runProject.sh
@@ -1,13 +1,25 @@
 # build images if necessary
 echo "BUILD IMAGES IF NECESSARY BEFORE THIS"
 
+# map size for elasticsearch
+echo "set sysctl max_map_count"
+sudo sysctl -w vm.max_map_count=262144
+
+# stop and remove all container
+echo "stopping and erasing working containers"
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq) 
+
 # run the portainer (name not from iosr)
+echo "running the Portainer"
 docker run -d -p 9000:9000  --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer --admin-password '$2y$05$4VN2X91zmAEqNcVnkjezDuHBte6h2zB9nZWcwNvdl0PUy.bePB9.O'
 
 # run pumba 
-docker run -d -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba pumba --random --interval 1m kill --signal SIGKILL re2:^iosr
+#echo "running Pumba"
+#docker run -d -v /var/run/docker.sock:/var/run/docker.sock gaiaadm/pumba pumba --random --interval 1m kill --signal SIGKILL re2:^iosr
 
 # run the docker-compose 
+echo "Docker-compose up"
 docker-compose build
 docker-compose up
 
